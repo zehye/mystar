d677d467005e7e68e598b2ac7947a1aacdc7d7a6
jihye <zehye.01@gmail.com>
2020-05-27 10:48:14 +0900

    first commit

---
+++ MyStar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyStar
+//
+//  Created by 졔님 on 2020/05/20.
+//  Copyright © 2020 zehye. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
